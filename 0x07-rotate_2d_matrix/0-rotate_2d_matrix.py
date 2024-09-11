528a7e0ef896d54e4734f9ae18811d9c8721ae81
vale-hash <newlechim@gmail.com>
2024-09-11 12:39:49 +0100

    pycoded

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,15 +2,20 @@
 """
 Script witha function to transpose a 2d matrix
 """
+
+
 def rotate_2d_matrix(matrix):
     """
     Function that rotates a 2d matrix by 90 degrees
     """ 
+    
     for i in range(len(matrix)):
         matrix[i].reverse()
+    
     for i in range(len(matrix)):
         for j in range(i, len(matrix)):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
     for i in range(len(matrix)):
         matrix[i].reverse()
     matrix.reverse()
