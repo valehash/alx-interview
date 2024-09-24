5d4783429eb90c7acdb8c3d9f9c63cba538e4d2d
vale-hash <newlechim@gmail.com>
2024-09-24 17:31:03 +0100

    pascal question

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Script ot create pascals triangle with arrays
+"""
+from typing import List
+def pascal_triangle(n:int) -> List[int]:
+"""Function to calculate and display pascals triangle"""
+    res = [[1]]
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
